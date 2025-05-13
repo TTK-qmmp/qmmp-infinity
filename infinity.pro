4dfd8fbcaf0928426569252e3895041b256ba492
Greedysky <greedysky@163.com>
2025-05-13 17:09:31 +0800

    feat: Add visual module

---
+++ infinity.pro
@@ -0,0 +1,39 @@
+HEADERS += infinitywidget.h \
+           visualinfinityfactory.h
+
+SOURCES += infinitywidget.cpp \
+           visualinfinityfactory.cpp
+
+#CONFIG += BUILD_PLUGIN_INSIDE
+contains(CONFIG, BUILD_PLUGIN_INSIDE){
+    include($$PWD/../../plugins.pri)
+    TARGET = $$PLUGINS_PREFIX/Visual/infinity
+
+    unix{
+        target.path = $$PLUGIN_DIR/Visual
+        INSTALLS += target
+    }
+}else{
+    QT += widgets
+    CONFIG += warn_off plugin lib thread link_pkgconfig c++11
+    TEMPLATE = lib
+
+    unix{
+        equals(QT_MAJOR_VERSION, 5){
+            QMMP_PKG = qmmp-1
+        }else:equals(QT_MAJOR_VERSION, 6){
+            QMMP_PKG = qmmp
+        }else{
+            error("Unsupported Qt version: 5 or 6 is required")
+        }
+        
+        PKGCONFIG += $${QMMP_PKG}
+
+        PLUGIN_DIR = $$system(pkg-config $${QMMP_PKG} --variable=plugindir)/Visual
+        INCLUDEPATH += $$system(pkg-config $${QMMP_PKG} --variable=prefix)/include
+
+        plugin.path = $${PLUGIN_DIR}
+        plugin.files = lib$${TARGET}.so
+        INSTALLS += plugin
+    }
+}
---
+++ visualinfinityfactory.cpp
@@ -0,0 +1,41 @@
+#include "visualinfinityfactory.h"
+#include "infinitywidget.h"
+
+#include <QMessageBox>
+
+VisualProperties VisualInfinityFactory::properties() const
+{
+    VisualProperties properties;
+    properties.name = tr("Infinity Plugin");
+    properties.shortName = "infinity";
+    properties.hasAbout = true;
+    return properties;
+}
+
+Visual *VisualInfinityFactory::create(QWidget *parent)
+{
+    return new InfinityWidget(parent);
+}
+
+#if (QMMP_VERSION_INT < 0x10700) || (0x20000 <= QMMP_VERSION_INT && QMMP_VERSION_INT < 0x20200)
+QDialog *VisualInfinityFactory::createConfigDialog(QWidget *parent)
+#else
+QDialog *VisualInfinityFactory::createSettings(QWidget *parent)
+#endif
+{
+    Q_UNUSED(parent);
+    return nullptr;
+}
+
+void VisualInfinityFactory::showAbout(QWidget *parent)
+{
+    QMessageBox::about(parent, tr("About Infinity Visual Plugin"),
+                       tr("Qmmp Infinity Visual Plugin") + "\n" +
+                       tr("This plugin adds Infinity visualization") + "\n" +
+                       tr("Written by: Greedysky <greedysky@163.com>"));
+}
+
+QString VisualInfinityFactory::translation() const
+{
+    return QString();
+}
