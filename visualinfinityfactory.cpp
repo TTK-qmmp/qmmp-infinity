4db6b32ca5a8f4d323f12b55a5aafb899d0ccdce
Greedysky <greedysky@163.com>
2025-11-07 15:16:52 +0800

    feat: Support for qmmp 0.x Qt4

--- visualinfinityfactory.cpp
+++ visualinfinityfactory.cpp
@@ -39,3 +39,8 @@ QString VisualInfinityFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(infinity, VisualInfinityFactory)
+#endif
